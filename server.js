16c8fe9d3e6e0310f72438fa315255b9420f890f
OmarCharlesGedeon <charles@effortlesshost.com>
2019-01-21 18:15:49 -0800

    formatting docs

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ let express = require('express');
 let bodyParser = require('body-parser'); 
 let exphbs = require('express-handlebars'); 
 
-var db = require("./models"); 
 let cheerio = require('cheerio'); 
 let mongoose = require('mongoose');
 
 let PORT = process.env.PORT || 8080; // Set Default Port for Express and Heroku
 let app = express(); // Initialize Express
+
+var db = require("./models"); 
 
 // app.use(logger("dev")); // Use morgan logger for logging requests
 app.use(bodyParser.urlencoded({ extended: false })); // Use body-parser for handling form submissions
