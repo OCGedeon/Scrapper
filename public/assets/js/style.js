42876f4ee7fa84d470eb98b21f1c4bc66bc037bd
OmarCharlesGedeon <charles@effortlesshost.com>
2019-01-21 17:40:35 -0800

    styling files css and function style.js

---
+++ public/assets/js/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  h4: {
+    color: 'white'
+  },
+  hover: {
+    backgroundColor: "rgb(70, 125, 196)"
+  },
+  'hoverRed:hover': {
+    color: rgb(27, 36, 111)
+  },
+  'hover:hover': {
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  }
+});
